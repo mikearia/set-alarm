de6604b9ae24376748a129d3c80ba70367cd762d
unknown <mikeariads@gmail.com>
2016-04-27 10:04:11 +0700

    set alarm with time picker

--- app/src/main/java/com/codelabs/alarm/MainActivity.java
+++ app/src/main/java/com/codelabs/alarm/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationBuilderWithBuilderAccessor;
@@ -13,12 +14,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
-    Button showNotificationBut, stopNotificationBut, alertButton;
+    TimePicker timePicker;
+    Switch turnAlarm;
+
+    int hour;
+    int minutes;
+
     NotificationManager notificationManager;
     boolean isNotificActive = false;
     int notifID = 33;
@@ -26,11 +37,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final Calendar calendar = Calendar.getInstance();
+        hour = calendar.get(calendar.HOUR_OF_DAY);
+        minutes = calendar.get(calendar.MINUTE);
 
-        showNotificationBut = (Button) findViewById(R.id.button_show);
-        stopNotificationBut = (Button) findViewById(R.id.button_stop);
-        alertButton = (Button) findViewById(R.id.button_alert);
+        setContentView(R.layout.activity_main);
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        turnAlarm = (Switch) findViewById(R.id.switch_turnalarm);
+
+        turnAlarm.setChecked(true);
+        turnAlarm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    turnAlarm.setText("Turn Alarm On");
+                    setAlarm();
+                    showNotification(getCurrentFocus());
+                } else {
+                    turnAlarm.setText("Turn Alarm Off");
+                    stopNotification();
+                }
+            }
+        });
     }
 
     public void showNotification(View view) {
@@ -39,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 .setContentTitle("Message")
                 .setContentText("New Message")
                 .setTicker("Alert New Message")
-                .setSmallIcon(R.drawable.default_avatar);
+                .setSmallIcon(R.drawable.ic_alarm_on_white_24dp);
 
         Intent moreInfoIntent = new Intent(this, MoreInfoNotification.class);
         TaskStackBuilder tStackBuilder = TaskStackBuilder.create(this);
@@ -53,14 +81,14 @@ public class MainActivity extends AppCompatActivity {
         isNotificActive = true;
     }
 
-    public void stopNotification(View view) {
+    public void stopNotification() {
         if (isNotificActive){
             notificationManager.cancel(notifID);
         }
     }
 
-    public void setAlarm(View view){
-        Long alertTime = new GregorianCalendar().getTimeInMillis()+5*1000;
+    public void setAlarm(){
+        Long alertTime = new GregorianCalendar().getTimeInMillis()+(hour+minutes)*1000;
 
         Intent alertIntent = new Intent(this, AlertReceiver.class);
 
@@ -70,5 +98,10 @@ public class MainActivity extends AppCompatActivity {
         alarmManager.set(AlarmManager.RTC_WAKEUP, alertTime,
                 PendingIntent.getBroadcast(this, 1, alertIntent,
                         PendingIntent.FLAG_UPDATE_CURRENT));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Toast.makeText(this,String.valueOf(hour) + String.valueOf(minute) , Toast.LENGTH_SHORT).show();
     }
 }
